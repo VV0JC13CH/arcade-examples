c9a58389f8aee916ee614828f68c260c8544290d
wojciech <wojciech@bitstudio.dev>
2019-06-30 00:37:42 +0200

    fixed FPS counter

--- examples/00_new_arcade_project/new_arcade_project.py
+++ examples/00_new_arcade_project/new_arcade_project.py
@@ -1,18 +1,17 @@
 """
 Arcade Starting Template from bitStudio.dev
 Added few useful methods and variables:
-
 +on_resize method
 +set_viewport method
 +on_mouse_scroll method
 +set_mouse_visible method
-
 +resizable
 +fullscreen
 +update_rate
 +antialiasing
 """
 import arcade
+import timeit
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -24,20 +23,27 @@ SCREEN_FULLSCREEN = False
 VISIBLE_MOUSE = True
 # 1/20 means constant 60 fps
 SCREEN_UPDATE_RATE = 1/60
-SCREEN_ANTIALIASING = True
 
 class MyGame(arcade.Window):
     """
     Main application class.
-
     NOTE: Go ahead and delete the methods you don't need.
     If you do need a method, delete the 'pass' and replace it
     with your own code. Don't leave 'pass' in this program.
     """
 
-    def __init__(self, width, height, title, resizable, fullscreen, update_rate, antialiasing):
-        super().__init__(width,height, title, resizable, fullscreen, update_rate, antialiasing)
+    def __init__(self, width, height, title, resizable, fullscreen):
+        super().__init__(width,height, title, resizable, fullscreen)
+
+        self.processing_time = 0
+        self.draw_time = 0
+        self.frame_count = 0
+        self.fps_start_timer = None
+        self.fps = None
+
+        self.set_update_rate(SCREEN_UPDATE_RATE)
         self.set_mouse_visible(VISIBLE_MOUSE)
+        self.font_color=arcade.color.BLACK
         arcade.set_background_color(arcade.color.BATTLESHIP_GREY)
 
         # If you have sprite lists, you should create them here,
@@ -47,16 +53,34 @@ class MyGame(arcade.Window):
         # Create your sprites and sprite lists here
         pass
 
+    def calculate_fps(self):
+        if self.frame_count % 60 == 0:
+            if self.fps_start_timer is not None:
+                total_time = timeit.default_timer() - self.fps_start_timer
+                self.fps = 60 / total_time
+            self.fps_start_timer = timeit.default_timer()
+        self.frame_count += 1
+
+    def on_draw_hud(self, draw_start_time):
+        # Display timings
+        output = f"Processing time: {self.processing_time:.3f}"
+        arcade.draw_text(output, 10, (SCREEN_HEIGHT-20), self.font_color, 16)
+
+        output = f"Drawing time: {self.draw_time:.3f}"
+        arcade.draw_text(output, 10 , (SCREEN_HEIGHT-40), self.font_color, 16)
+
+        if self.fps is not None:
+            output = f"FPS: {self.fps:.0f}"
+            arcade.draw_text(output, 10 , (SCREEN_HEIGHT-60), self.font_color, 16)
+
+        self.draw_time = timeit.default_timer() - draw_start_time
+
     def on_draw(self):
-        """
-        Render the screen.
-        """
-
-        # This command should happen before we start drawing. It will clear
-        # the screen to the background color, and erase what we drew last frame.
+        # Start timing how long this takes
+        draw_start_time = timeit.default_timer()
+        self.calculate_fps()
         arcade.start_render()
-
-        # Call draw() on all your sprite lists below
+        self.on_draw_hud(draw_start_time)
 
     def set_viewport(self, left, right, bottom, top):
         """
@@ -83,7 +107,6 @@ class MyGame(arcade.Window):
     def on_key_press(self, key, key_modifiers):
         """
         Called whenever a key on the keyboard is pressed.
-
         For a full list of keys, see:
         http://arcade.academy/arcade.key.html
         """
@@ -116,7 +139,6 @@ class MyGame(arcade.Window):
     def on_mouse_scroll(self, x, y, scroll_x, scroll_y):
         """
         User moves the scroll wheel.
-
         """
         pass
 
@@ -127,9 +149,7 @@ def main():
                   SCREEN_HEIGHT,
                   SCREEN_TITLE,
                   SCREEN_RESIZABLE,
-                  SCREEN_FULLSCREEN,
-                  SCREEN_UPDATE_RATE,
-                  SCREEN_ANTIALIASING)
+                  SCREEN_FULLSCREEN)
     game.setup()
     arcade.run()
 
