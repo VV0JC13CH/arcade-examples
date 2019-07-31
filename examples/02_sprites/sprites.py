cc1a4f1ff840b75d6b31b4dbc8bbe3480cd14130
wojciech <wojciech@bitstudio.dev>
2019-07-31 14:32:13 +0200

    added 02_sprites example

---
+++ examples/02_sprites/sprites.py
@@ -0,0 +1,91 @@
+"""
+The Python Arcade Library Examples by bitStudio.dev
+Example with sprites.
+
+Sprites license: Copyright 1995 Creative Edge Software Ltd. All rights reserved.
+Granted permission to use copyrighted works for education purposes only.
+https://github.com/bitStudioDev/arcade.py-examples"""
+
+import arcade
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "The Python Arcade Library Examples - Sprites"
+
+# Change default values
+SCREEN_RESIZABLE = False
+SCREEN_FULLSCREEN = False
+VISIBLE_MOUSE = True
+
+
+class SpritesExample(arcade.Window):
+    """
+    Main application class.
+    NOTE: Go ahead and delete the methods you don't need.
+    If you do need a method, delete the 'pass' and replace it
+    with your own code. Don't leave 'pass' in this program.
+    """
+
+    def __init__(self, width, height, title, fullscreen, resizable):
+        super().__init__(width, height, title, fullscreen, resizable)
+        self.set_mouse_visible(VISIBLE_MOUSE)
+        self.font_color=arcade.color.BLACK
+        arcade.set_background_color(arcade.color.WHITE)
+
+        # If you have sprite lists, you should create them here,
+        # and set them to None
+        self.animated_bg_island_sprite_list = None
+        self.animated_bg_island_sprite = None
+        self.static_house_sprite = None
+
+    def setup(self):
+        # Create your sprites and sprite lists here
+        self.animated_bg_island_sprite = arcade.AnimatedTimeSprite(
+            center_x=self.width / 2,
+            center_y=self.height / 2,
+        )
+        self.animated_bg_island_sprite.textures = []
+        self.animated_bg_island_sprite.textures.append(arcade.load_texture("02_sprites_island1.png", scale=2))
+        self.animated_bg_island_sprite.textures.append(arcade.load_texture("02_sprites_island2.png", scale=2))
+        self.animated_bg_island_sprite.textures.append(arcade.load_texture("02_sprites_island3.png", scale=2))
+        self.animated_bg_island_sprite.textures.append(arcade.load_texture("02_sprites_island2.png", scale=2))
+
+        self.animated_bg_island_sprite_list = arcade.SpriteList()
+        self.animated_bg_island_sprite_list.append(self.animated_bg_island_sprite)
+
+        self.static_house_sprite = arcade.load_texture("02_sprites_house.png")
+
+    def on_draw(self):
+        arcade.start_render()
+        self.animated_bg_island_sprite_list.draw()
+        arcade.draw_texture_rectangle(
+            center_x=self.width / 2,
+            center_y=self.height / 2,
+            width=98,
+            height=98,
+            texture=self.static_house_sprite,
+            angle=0)
+        arcade.finish_render()
+
+    def update(self, delta_time):
+        """
+        All the logic to move, and the game logic goes here.
+        Normally, you'll call update() on the sprite lists that
+        need it.
+        """
+        self.animated_bg_island_sprite_list.update_animation()
+
+
+def main():
+    """ Main method """
+    example = SpritesExample(SCREEN_WIDTH,
+                             SCREEN_HEIGHT,
+                             SCREEN_TITLE,
+                             SCREEN_FULLSCREEN,
+                             SCREEN_RESIZABLE)
+    example.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
